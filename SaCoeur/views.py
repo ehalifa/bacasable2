3bea38da7fa9fd0e4d2dd60f4e0f7e654d32d8ba
kardelen-karatas <kardelen.karatas@gmail.com>
2017-12-17 22:08:12 +0100

    repo cleaning and some additions

--- SaCoeur/views.py
+++ SaCoeur/views.py
@@ -2,10 +2,33 @@ from django.shortcuts import render
 from .models import *
 from django.views.generic import *
 from django.urls import reverse, reverse_lazy
+from django.db.models import Count,Sum, Avg
 
-# Create your views here.
+# je dois pouvoir visualiser les stocks des produits dans chaque building
+# 1) liste des buildings (order by type of building) dans un premeir temps  "BuildingList(ListView)"
+# 2) ensuite liste des produits existants (product description with count query on product class) pour chaque building "ProductList(ListView)"
+# 3) finallement on doit pouvoir afficher la description de chaque produit "ProductDetail(DetailView)"
+
+class BuildingList(ListView):
+    model = Building
+    template_name = "building_list.html"
+    context_object_name = "building_list"
+    queryset = Building.objects.values("name","type","capacity").order_by("type").annotate( total_product = Count("product__id_product_description__name"))
+"""
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.values("id").order_by()
+        return queryset
+    #queryset = Building.objects.values('product__id_product_description__name').annotate(total = Sum('capacity'),bagnb = Count('name'))
+"""
+
 class ProductList(ListView):
-    model = Product
+    model = Product_Description
     template_name = "product_list.html"
-    context_object_name = "product"
-    list = Product.objects.values('id_product_description')
+    context_object_name = "product_list"
+    queryset = Product_Description.objects.values('product__id_product_description__brand').annotate(bagnb = Count('id'))
+
+"""
+class ProductDetailList(DetailView):
+    XXXX
+"""
