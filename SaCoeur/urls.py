7131699b37e5d027a65979c16529769d343a8ee7
kardelen-karatas <kardelen.karatas@gmail.com>
2017-12-20 15:24:54 +0100

    Product Detail Page

--- SaCoeur/urls.py
+++ SaCoeur/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     url(r'^building/$', BuildingList.as_view(), name= 'building'),
     url (r'^products/$', ProductList.as_view(), name='products'),
     url (r'^products/(?P<product_type>[\w]*)$', ProductList.as_view(), name='products'),
+    url (r'^products/details/(?P<pk>[-\d]+)$', ProductDetail.as_view (), name='product_detail'),
+    #url (r'^products/new_product/$', CreateProduct.as_view(), name='new_product'),
+
 ]
--- SaCoeur/views.py
+++ SaCoeur/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.views.generic import *
-from django.db.models import Count,Sum, Avg
-from django.shortcuts import get_object_or_404
-from django.db.models import Q
-
-# je dois pouvoir visualiser les stocks des produits dans chaque building
-# 1) liste des buildings (order by type of building) dans un premeir temps  "BuildingList(ListView)"
-# 2) ensuite liste des produits existants (product description with count query on product class) pour chaque building "ProductList(ListView)"
-# 3) finallement on doit pouvoir afficher la description de chaque produit "ProductDetail(DetailView)"
+from django.db.models import Count
 
 class BuildingList(ListView):
     model = Building
@@ -19,8 +12,6 @@ class BuildingList(ListView):
         queryset = super().get_queryset()
         queryset = queryset.values("name","type","capacity").order_by("type").annotate( total_product = Count("product__id_product_description__name"))
         return queryset
-
-
 
 class ProductList(ListView):
     model = Product
@@ -35,3 +26,12 @@ class ProductList(ListView):
         if query:
             result = result.filter(id_product_description__name__icontains = query)
         return result
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+    context_object_name = "product_detail"
+
+class CreateProduct(CreateView):
+    model = Product
+    fields = ('arrival_date', 'id_building', 'id_product_description')
