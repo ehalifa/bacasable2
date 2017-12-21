e5e78bfed78353fcc84cef3b042ae4ec1551a810
el-amin <halifa.elamin@gmail.com>
2017-12-21 10:30:15 +0100

    Correction Urls

--- SaCoeur/urls.py
+++ SaCoeur/urls.py
@@ -3,11 +3,11 @@ from .views import *
 
 urlpatterns = [
     url(r'^building/$', BuildingList.as_view(), name= 'building'),
-    url (r'^products/$', ProductList.as_view(), name='products'),
-    url (r'^products/(?P<product_type>[\w]*)$', ProductList.as_view(), name='products'),
-    url (r'^products/details/(?P<pk>[-\d]+)$', ProductDetail.as_view (), name='product_detail'),
-    url(r'^productstocrud/$', ProductToCRUD.as_view(), name='producttocrud'),
-    url (r'^products/details/new/$', CreateProduct.as_view(), name='new_product'),
-    url (r'^products/details/update/(?P<pk>[\d]+)$', UpdateProduct.as_view (), name='update_product'),
-    url (r'^products/details/delete/(?P<pk>[\d]+)$', DeleteProduct.as_view (), name='delete_product'),
+    url(r'^product/list_stock/$', ProductList.as_view(), name='products'),
+    url(r'^product/list_stock/(?P<product_type>[\w]*)$', ProductList.as_view(), name='products'),
+    url(r'^product/list_crud/$', ProductToCRUD.as_view(), name='producttocrud'),
+    url(r'^product/new/$', CreateProduct.as_view(), name='new_product'),
+    url(r'^product/update/(?P<pk>[\d]+)$', UpdateProduct.as_view (), name='update_product'),
+    url(r'^product/delete/(?P<pk>[\d]+)$', DeleteProduct.as_view (), name='delete_product'),
+    url(r'^product/detail/(?P<pk>[-\d]+)$', ProductDetail.as_view (), name='product_detail'),
 ]
